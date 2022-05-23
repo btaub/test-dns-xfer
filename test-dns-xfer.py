b220c582086f25d27781a39270996ac02141b01c
Beau Taub <beau.taub@gmail.com>
2022-05-23 00:37:55 -0400

    Add timestamp for output file name
    make verbose option work right

--- test-dns-xfer.py
+++ test-dns-xfer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 '''
 # Required: dnspython:
 sh-3.2$ pip install dnspython
@@ -10,12 +11,13 @@ import sys
 import socket
 import re
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Test a domain for zone xfer" \
         ,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d","--domain",help="The domain you want record(s) for",default="example.org")
-parser.add_argument("-o","--output",help="Output file to write to")
-parser.add_argument("-v","--verbose",action="store_false",help="Verbose output")
+parser.add_argument("-o","--output",help="Create output file for transfer", action="store_true")
+parser.add_argument("-v","--verbose",action="store_true",help="Verbose output")
 args = parser.parse_args()
 
 def get_nameservers(zonename):
@@ -33,15 +35,21 @@ def get_zone_xfer(zonename):
         for nameserver in get_nameservers(zonename):
             if args.verbose:
                 print("[+] Testing nameserver: %s" % (nameserver))
-            nameserver = dns.resolver.resolve(str(nameserver), rdtype=dns.rdatatype.A)
-            the_xfer = dns.zone.from_xfr(dns.query.xfr(str(nameserver[0]), zonename))
+            nameserver_ip = dns.resolver.resolve(str(nameserver), rdtype=dns.rdatatype.A)
+            the_xfer = dns.zone.from_xfr(dns.query.xfr(str(nameserver_ip[0]), zonename))
             names = the_xfer.nodes.keys()
             for n in names:
-                print(the_xfer[n].to_text(n))
-                if args.output:
-                    with open(args.output,'a+') as f:
+               if args.output:
+                   dt = datetime.now()
+                   ts = dt.strftime("%d%m%Y-%I%M%s")
+                   with open(args.domain+"-"+str(nameserver)+ts+".txt",'a+') as f:
                         f.write("DOMAIN: %s\n" % args.domain)
                         f.write(the_xfer[n].to_text(n))
+                   if args.verbose:
+                        print(the_xfer[n].to_text(n))
+
+            print("Transfer from nameserver %s successful, check output log for details" % nameserver)
+
     except dns.xfr.TransferError as e:
          print("ERR: %s" %e)
 
